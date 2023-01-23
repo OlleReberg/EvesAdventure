5da7e437f0d0bf4f1499862dc2979b897aa6521d
OlleReberg <ollereberg@hotmail.com>
2023-01-23 16:29:31 +0100

    Flawed animations set up

--- Eve's Adventure/Assets/Scripts/PlayerMovement.cs
+++ Eve's Adventure/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting.Dependencies.NCalc;
@@ -7,10 +8,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody _rb;
-    public float baseMoveSpeed = 10f;
+    private Rigidbody _rb;
+    private Animator _animator;
+    public float baseMoveSpeed = 5f;
     public float boostMoveSpeed = 15f;
     public float boostDuration = 10f;
     public Vector2 rotation;
@@ -18,42 +23,24 @@ public class PlayerMovement : MonoBehaviour
     public float smoothing = 5f;
 
     private bool isBoosting = false;
+    private bool isRunning = false;
     private float boostTimer = 0f;
 
     private void Start()
     {
-        _rb = GetComponent<Rigidbody>(); // Get the Rigidbody component from the object and assign it to the _rb variable
+        _rb = GetComponent<Rigidbody>(); // Get Rigidbody and assign it to variable
+        _animator = GetComponent<Animator>(); //Get Animator and assign it to variable
         Cursor.lockState = CursorLockMode.Locked; // lock the cursor within the game window
         Cursor.visible = false; // make the cursor invisible
     }
 
     private void Update()
     {
-        // Check for boost input
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            isBoosting = true; // set the isBoosting variable to true
-            boostTimer = 0f; // reset the boost timer
-        }
-
-        rotation.x += Input.GetAxis("Mouse X") * sensitivity; // get horizontal mouse input and multiply by sensitivity
-        rotation.y += Input.GetAxis("Mouse Y") * sensitivity; // get vertical mouse input and multiply by sensitivity
-        //TODO: Make rotation based on camera rather than player, alt based on player head movement
-        rotation.y = Mathf.Clamp(rotation.y, -50f, 50f); // limit the rotation on the y-axis
-        transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(-rotation.y, rotation.x, 0), Time.deltaTime * smoothing); // smoothly rotate the player based on the rotation vector
-
-        // Update boost timer
-        if (isBoosting)
-        {
-            boostTimer += Time.deltaTime; // increment the boost timer
-
-            // End boost if duration has been reached
-            if (boostTimer >= boostDuration)
-            {
-                isBoosting = false; // reset isBoosting to false
-                boostTimer = 0f; // reset the boost timer
-            }
-        }
+        // rotation.x += Input.GetAxis("Mouse X") * sensitivity; // get horizontal mouse input and multiply by sensitivity
+         //rotation.y += Input.GetAxis("Mouse Y") * sensitivity; // get vertical mouse input and multiply by sensitivity
+        // //TODO: Make rotation based on camera rather than player, alt based on player head movement
+         //rotation.y = Mathf.Clamp(rotation.y, -50f, 50f); // limit the rotation on the y-axis
+         //transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(-rotation.y, rotation.x, 0), Time.deltaTime * smoothing); // smoothly rotate the player based on the rotation vector
     }
 
     private void FixedUpdate()
@@ -62,10 +49,49 @@ public class PlayerMovement : MonoBehaviour
         float horInput = Input.GetAxis("Horizontal"); // get horizontal input
         float vertInput = Input.GetAxis("Vertical"); // get vertical input
         float boostInput = Input.GetAxis("RocketBoost"); // get boost input
+        Vector3 movement = new Vector3(horInput, boostInput, vertInput);
         
-        Vector3 movement = new Vector3(horInput, boostInput, vertInput); // Calculate movement
+        // Check for boost input
+        if (MathF.Abs(_rb.velocity.y) < 0.01f && Input.GetButtonDown("RocketBoost"))
+        {
+            isBoosting = true; // set the isBoosting variable to true
+            boostTimer = 0f; // reset the boost timer
+            _animator.SetBool("Boosting", true);
+        }
+        
+        
+        // Update boost timer
+        if (isBoosting)
+        {
+            boostTimer += Time.deltaTime; // increment the boost timer
+            _rb.AddForce(movement * (isBoosting ? boostMoveSpeed : baseMoveSpeed), ForceMode.Acceleration);
+            // End boost if duration has been reached
+            if (boostTimer >= boostDuration)
+            {
+                isBoosting = false; // reset isBoosting to false
+                _animator.SetBool("Boosting", false);
+                boostTimer = 0f; // reset the boost timer
+            }
+        }
+        
 
-        // Apply movement to Rigidbody
-        _rb.AddForce(movement * (isBoosting ? boostMoveSpeed : baseMoveSpeed), ForceMode.Acceleration);
+        if (MathF.Abs(vertInput) > 0.01f)
+        {
+            //Move character
+            transform.rotation = Quaternion.LookRotation(new Vector3(-vertInput, 0f, 0f));
+            _rb.MovePosition(_rb.position - transform.forward * (baseMoveSpeed * Time.fixedDeltaTime));
+            
+            //Start run animation
+            if (!isRunning)
+            {
+                isRunning = true;
+                _animator.SetBool("Running", true);
+            }
+            else if (isRunning)
+            {
+                isRunning = false;
+                _animator.SetBool("Running", false);
+            }
+        }
     }
 }
