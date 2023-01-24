eed17eee43753256fe080e745a155421d634c9a0
OlleReberg <ollereberg@hotmail.com>
2023-01-24 11:58:51 +0100

    Camera fixed but animation is fucked and player runs backwards

--- Eve's Adventure/Assets/Scripts/PlayerMovement.cs
+++ Eve's Adventure/Assets/Scripts/PlayerMovement.cs
@@ -13,73 +13,69 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody _rb;
+    [Header("References")] 
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform playerObj;
+    [SerializeField] private Transform orientation;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float rotationSpeed;
+    
     private Animator _animator;
-    public float baseMoveSpeed = 5f;
-    public float boostMoveSpeed = 15f;
-    public float boostDuration = 10f;
-    public Vector2 rotation;
-    public float sensitivity = 3f;
-    public float smoothing = 5f;
+    [Header("Movement")]
+    [SerializeField] private float baseMoveSpeed = 5f;
+    [SerializeField] private float boostMoveSpeed = 15f;
+    [SerializeField] private float boostDuration = 10f;
 
+
+    [SerializeField] private float groundDrag;
+    [SerializeField] private float playerHeight;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+    
     private bool isBoosting = false;
     private bool isRunning = false;
     private float boostTimer = 0f;
-
+    private Vector3 moveDirection;
+    
+    // Get movement input
+    float horInput; // get horizontal input
+    float vertInput; // get vertical input
+    
     private void Start()
     {
-        _rb = GetComponent<Rigidbody>(); // Get Rigidbody and assign it to variable
+        rb = GetComponent<Rigidbody>(); // Get Rigidbody and assign it to variable
         _animator = GetComponent<Animator>(); //Get Animator and assign it to variable
         Cursor.lockState = CursorLockMode.Locked; // lock the cursor within the game window
         //Cursor.visible = false; // make the cursor invisible
     }
     
-    private void FixedUpdate()
+    private void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X"); // get horizontal mouse input
-        float mouseY = Input.GetAxis("Mouse Y"); // get vertical mouse input
-
-        // Calculate the new rotation for the player
-        Quaternion newRotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(-mouseY * sensitivity, mouseX * sensitivity, 0));
-
-        // Smoothly rotate the player towards the new rotation
-        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, smoothing * Time.deltaTime);
+        //Ground check
+        isGrounded = Physics.Raycast(transform.position,
+            Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        //Handle drag
+        if (isGrounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
         
-        // Get movement input
-        float horInput = Input.GetAxis("Horizontal"); // get horizontal input
-        float vertInput = Input.GetAxis("Vertical"); // get vertical input
-        float boostInput = Input.GetAxis("RocketBoost"); // get boost input
-        Vector3 movement = new Vector3(horInput, boostInput, vertInput);
+        //rotate orientation
+        Vector3 viewDirection = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDirection.normalized;
         
-        // Check for boost input
-        if (MathF.Abs(_rb.velocity.y) < 0.01f && Input.GetButtonDown("RocketBoost"))
-        {
-            isBoosting = true; // set the isBoosting variable to true
-            boostTimer = 0f; // reset the boost timer
-            _animator.SetBool("Boosting", true);
-        }
+        Vector3 inputDirection = orientation.forward * vertInput + orientation.right * horInput;
         
+        MovePlayer();
         
-        // Update boost timer
-        if (isBoosting)
-        {
-            boostTimer += Time.deltaTime; // increment the boost timer
-            _rb.AddForce(movement * (isBoosting ? boostMoveSpeed : baseMoveSpeed), ForceMode.Acceleration);
-            // End boost if duration has been reached
-            if (boostTimer >= boostDuration)
-            {
-                isBoosting = false; // reset isBoosting to false
-                _animator.SetBool("Boosting", false);
-                boostTimer = 0f; // reset the boost timer
-            }
-        }
-        
+        if (inputDirection != Vector3.zero)
+            playerObj.forward = Vector3.Slerp(playerObj.forward,
+                inputDirection.normalized, Time.deltaTime * rotationSpeed);
+        MyInput();
+        //Boost();
 
         if (MathF.Abs(vertInput) > 0.01f)
         {
-            //Move character
-            transform.rotation = Quaternion.LookRotation(new Vector3(-vertInput, 0f, 0f));
-            _rb.MovePosition(_rb.position - transform.forward * (baseMoveSpeed * Time.fixedDeltaTime));
             
             //Start run animation
             if (!isRunning)
@@ -93,5 +89,44 @@ public class PlayerMovement : MonoBehaviour
                 _animator.SetBool("Running", false);
             }
         }
+    }
+
+    private void Boost()
+    {
+        float boostInput = Input.GetAxis("RocketBoost"); // get boost input
+        Vector3 movement = new Vector3(horInput, boostInput, vertInput);
+        // Check for boost input
+        if (MathF.Abs(rb.velocity.y) < 0.01f && Input.GetButtonDown("RocketBoost"))
+        {
+            isBoosting = true; // set the isBoosting variable to true
+            boostTimer = 0f; // reset the boost timer
+            _animator.SetBool("Boosting", true);
+        }
+
+        // Update boost timer
+        if (isBoosting)
+        {
+            boostTimer += Time.deltaTime; // increment the boost timer
+            rb.AddForce(movement * (isBoosting ? boostMoveSpeed : baseMoveSpeed), ForceMode.Acceleration);
+            // End boost if duration has been reached
+            if (boostTimer >= boostDuration)
+            {
+                isBoosting = false; // reset isBoosting to false
+                _animator.SetBool("Boosting", false);
+                boostTimer = 0f; // reset the boost timer
+            }
+        }
+    }
+
+    private void MyInput()
+    {
+        horInput = Input.GetAxisRaw("Horizontal"); // get horizontal input
+        vertInput = Input.GetAxisRaw("Vertical"); // get vertical input
+    }
+
+    void MovePlayer()
+    {
+        moveDirection = orientation.forward * vertInput + orientation.right * horInput;
+        rb.AddForce(moveDirection.normalized * baseMoveSpeed, ForceMode.Force);
     }
 }
